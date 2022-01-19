9ae8fe65c6b98cbba12940e89e9045334e9c31e2
Nadezhda <chuhonina.88@yandex.ru>
2022-01-19 23:23:52 +0300

    script'ы

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,11 @@ let addButton = document.querySelector('.edit-button');
 let closeButton = document.querySelector('.close-button');
 let saveButton = document.querySelector('.popup__save-button');
 let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__count_name');
 let jobInput = document.querySelector('.popup__count_description');
+let profileName = document.querySelector('.profile-info__name');
+let profileJob = document.querySelector('.profile-info__description')
 
 function openPopup(addButton) {
   popup.classList.add('popup_opened');
@@ -21,5 +24,12 @@ closeButton.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove('popup_opened'); 
+  console.log(formSubmitHandler);  
 }
+
+console.log(nameInput.value);
+
+formElement.addEventListener('submit', formSubmitHandler);
