233ec3d46519619020e5a9ed17b2cd16856a11b9
Nadezhda <chuhonina.88@yandex.ru>
2022-02-03 10:50:21 +0300

    feat: добавить изображение

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,34 @@
-let addButton = document.querySelector('.profile__edit-button');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
+let formElement = document.querySelector('.popup__form_profile');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_description');
 let profileName = document.querySelector('.profile-info__name');
@@ -24,6 +51,78 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
-addButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+//ПР5
+const template = document.querySelector('.element__template').content;
+const list = document.querySelector('.elements__list');
+const addButton = document.querySelector('.profile__add-button');
+const formElementCards = document.querySelector('.popup__form_element');
+const placeInput = document.querySelector('.popup__input_type_place');
+const linkInput = document.querySelector('.popup__input_type_link');
+const popupPlace = document.querySelector('.popup_element');
+const closePlaceButton = document.querySelector('.popup__close-button_element');
+
+//шесть карточек при запуске страницы
+function renderCards(element) {
+  const newCard = template.querySelector('.element').cloneNode(true);
+
+  newCard.querySelector('.element__title').textContent = element.name;
+  newCard.querySelector('.element__image').alt = element.name;
+  newCard.querySelector('.element__image').src = element.link;
+
+  list.append(newCard);
+  addListener(newCard);
+}
+
+function render() {
+  initialCards.forEach(renderCards);
+}
+
+render();
+
+//открытие и закрытие попапа
+function openCardPopup() {  
+ popupPlace.classList.add('popup_opened');
+}
+
+function closeCardPopup() {
+  popupPlace.classList.remove('popup_opened');
+}
+
+//вызовы
+addButton.addEventListener('click', openCardPopup);
+closePlaceButton.addEventListener('click', closeCardPopup);
+formElementCards.addEventListener('submit', addNewPlace);
+
+//добавление фото
+function addNewPlace(evt) {
+  evt.preventDefault();
+
+  const userCard =  template.querySelector('.element').cloneNode(true);
+  userCard.querySelector('.element__title').textContent = placeInput.value;  
+  userCard.querySelector('.element__image').src = linkInput.value;
+
+  list.prepend(userCard);
+  addListener(userCard);  
+  closeCardPopup(); 
+  placeInput.value = '';
+  linkInput.value = '';
+}
+
+function addListener(el) {
+  el.querySelector('.element__like').addEventListener('click', handleLike);
+  el.querySelector('.element__delete').addEventListener('click', handleDelete);
+}
+
+//удаление фото
+function handleDelete(event) {
+  event.target.closest('.element').remove();
+}
+
+//like'и
+function handleLike(event) {
+ event.target.closest('.element__like').classList.toggle('element__like_active');
+}
